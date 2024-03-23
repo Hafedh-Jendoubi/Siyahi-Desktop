2e87c9a9ec576e0a9e1fec77795f31f58f9bdfcd
Hafedh-Jendoubi <hafedh.jendoubi@esprit.tn>
2024-03-23 22:37:45 +0100

    Init commit + User CRUD

---
+++ src/main/java/tn/esprit/Main.java
@@ -0,0 +1,10 @@
+package tn.esprit;
+
+import tn.esprit.models.User;
+import tn.esprit.services.UserService;
+
+public class Main {
+    public static void main(String[] args) {
+        UserService us = new UserService();
+    }
+}
