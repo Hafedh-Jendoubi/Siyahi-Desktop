f426dc80bf9d3875bb2003203004d1b943c1d2ca
skanderkacem <146791326+skanderkacem@users.noreply.github.com>
2024-04-21 22:48:35 +0200

    Sms
    1er API

--- src/main/java/tn/esprit/controllers/AjouterReponseCredit.java
+++ src/main/java/tn/esprit/controllers/AjouterReponseCredit.java
@@ -21,6 +21,10 @@ import java.sql.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+
 public class AjouterReponseCredit {
     @FXML
     private DatePicker DateConfirm;
@@ -40,6 +44,16 @@ public class AjouterReponseCredit {
     private final ReponseCreditService reponseCreditService = new ReponseCreditService();
     private final CreditService creditService = new CreditService();
     private int creditId;
+
+    // Configuration des identifiants Twilio
+    private static final String ACCOUNT_SID = "ACd4df0fc05c27caa57a1852fe00965381";
+    private static final String AUTH_TOKEN = "5fac94566b5277862bb218422aff5bf1";
+    private static final String TWILIO_NUMBER = "+12672824271";
+
+    // Initialisation de Twilio
+    static {
+        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+    }
 
     public void initData(int creditId) {
         this.creditId = creditId;
@@ -72,6 +86,7 @@ public class AjouterReponseCredit {
                 return;
             }
 
+            // Ajout de la réponse de crédit
             reponseCreditService.Add(new ReponseCredit(
                     Integer.parseInt(nbrconfirmTF.getText()),
                     DescriptionconfirmTF.getText(),
@@ -82,12 +97,25 @@ public class AjouterReponseCredit {
             showAlert(Alert.AlertType.INFORMATION, "Success", "Réponse de crédit ajoutée avec succès.");
             clearFields();
 
-            // Appeler directement la fonction RetourReponseLV
+            // Envoi du SMS via Twilio
+            sendSMS(selectedCredit);
+
+            // Redirection vers l'interface ListReponseCredit
             RetourReponseLV(event);
         } catch (Exception e) {
             showAlert(Alert.AlertType.ERROR, "Error", e.getMessage());
         }
     }
+
+    private void sendSMS(Credit selectedCredit) {
+        String message = "Votre demande de crédit a été traitée avec succès. Veuillez consulter notre site. Notez que vous ne pouvez pas modifier ni supprimer votre demande actuellement.";
+        Message.creator(
+                new PhoneNumber("+21658405717"), // Numéro du destinataire new PhoneNumber("votre_numero_destinataire"),
+                new PhoneNumber(TWILIO_NUMBER), // Numéro Twilio
+                message
+        ).create();
+    }
+
 
     @FXML
     void RetourReponseLV(ActionEvent event) {
